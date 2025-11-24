b91bda02372d92b45d34bc0d347a7f239237f001
heyjaywilson <heyjay@cctplus.dev>
2025-11-24 14:40:23 -0800

    Initial Commit

---
+++ ReleaseKit/ReleaseKit.swift
@@ -0,0 +1,9 @@
+//
+//  ReleaseKit.swift
+//  ReleaseKit
+//
+//  Created by Jay Wilson on 11/24/25.
+//
+
+import Foundation
+
---
+++ ReleaseKitTests/ReleaseKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaseKitTests.swift
+//  ReleaseKitTests
+//
+//  Created by Jay Wilson on 11/24/25.
+//
+
+import Testing
+@testable import ReleaseKit
+
+struct ReleaseKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
